4e8e99ee509c0bd505a46946c3aec713a34f302d
YasinVaran3 <WendyB1101@outlook.com>
2026-09-18 03:46:17 +0300

    fix error for vercel deploy

--- work-project/src/main.jsx
+++ work-project/src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import '@fontsource-variable/fraunces/full.css'
-import '@fontsource-variable/fraunces/full-italic.css'
+import '@fontsource-variable/fraunces'
 import '@fontsource-variable/dm-sans'
 import '@fontsource/caveat/500.css'
 import '@fontsource/caveat/700.css'
